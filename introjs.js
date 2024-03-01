4ea17d25b83604241c16737eda017241f3ecbd0b
Rituraj Sinha Mahapatra <67279998+riturajmahapatra@users.noreply.github.com>
2024-03-01 22:48:53 +0530

    Add files via upload

--- introjs.js
+++ introjs.js
@@ -69,3 +69,73 @@ if (age < 18) {
 }
 
 /* exercise 5 */
+
+// Make a keyless car EVEN BETTER!
+// We are improving our car from previous exercise now.
+
+/* Solutions to future exercises will be in the .js file (meaning Javascript file) from now on. */
+
+//1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+function checkDriverAge() {
+  var age = prompt('What is your age?');
+  if (Number(age) < 18) {
+    alert('Sorry, you are too yound to drive this car. Powering off');
+  } else if (Number(age) > 18) {
+    alert('Powering On. Enjoy the ride!');
+  } else if (Number(age) === 18) {
+    alert('Congratulations on your first year of driving. Enjoy the ride!');
+  }
+}
+
+checkDriverAge();
+
+//2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
+
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
+/* 
+checkDriverAge(92);
+it returns "Powering On. Enjoy the ride!" */
+
+function checkDriverAge2(age) {
+  if (Number(age) < 18) {
+    console.log('Sorry, you are too yound to drive this car. Powering off');
+  } else if (Number(age) > 18) {
+    console.log('Powering On. Enjoy the ride!');
+  } else if (Number(age) === 18) {
+    console.log(
+      'Congratulations on your first year of driving. Enjoy the ride!'
+    );
+  }
+}
+
+checkDriverAge2(98);
+
+/* exercise 6 */
+// using this array,
+var array = ['Banana', 'Apples', 'Oranges', 'Blueberries'];
+
+// 1. Remove the Banana from the array.
+array.shift();
+// 2. Sort the array in order.
+array.sort(); // "Apples"1: "Banana"2: "Blueberries"3: "Oranges"length: 4[[Prototype]]: Array(0)
+// 3. Put "Kiwi" at the end of the array.
+array.push('Kiwi');
+// 4. Remove "Apples" from the array.
+array.splice(1, 1);
+/* 
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+var nwe = array.splice(1,1);
+console.log(array) 
+*/
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+array.reverse();
+/* you should have at the end:
+["Kiwi", "Oranges", "Blueberries"] */
+
+// using this array,
+var array2 = ['Banana', ['Apples', ['Oranges'], 'Blueberries']];
+// access "Oranges".
+array2[1][1][0];
