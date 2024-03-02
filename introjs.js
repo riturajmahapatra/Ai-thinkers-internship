3a915b6ab3e24e38a9300d4078c1f53b250bef52
Rituraj Sinha Mahapatra <67279998+riturajmahapatra@users.noreply.github.com>
2024-03-02 23:20:10 +0530

    Add files via upload

--- introjs.js
+++ introjs.js
@@ -139,3 +139,23 @@ array.reverse();
 var array2 = ['Banana', ['Apples', ['Oranges'], 'Blueberries']];
 // access "Oranges".
 array2[1][1][0];
+
+/* exercise 7 */
+// Create an object and an array which we will use in our facebook exercise.
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+var database = {
+  username: 'Rituraj',
+  password: 'Kbae@235',
+};
+
+var newsfeed = [
+  { username: 'Tiria', timeline: 'rescue' },
+  { username: 'Bafri', timeline: 'search' },
+  { username: 'Hidins', timeline: 'protect' },
+];
